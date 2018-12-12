df2afb8edf7d48becf4b610c563aeff76bcb381b
trembear <1424479171@qq.com>
2018-12-12 17:56:27 +0800

    增加pageHelper

--- bookinfo-service/src/test/java/com/trembear/bookinfo/BookinfoApplicationTests.java
+++ bookinfo-service/src/test/java/com/trembear/bookinfo/BookinfoApplicationTests.java
@@ -1,8 +1,14 @@
 package com.trembear.bookinfo;
 
+import com.alibaba.fastjson.JSON;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.trembear.bookinfo.config.RedisConfig;
 import com.trembear.bookinfo.dao.BCAdminMapper;
 import com.trembear.bookinfo.dao.BookInfoDao;
+import com.trembear.bookinfo.entity.BCAdmin;
+import com.trembear.bookinfo.entity.BCAdminExample;
 import com.trembear.bookinfo.entity.BookInfo;
 import com.trembear.bookinfo.entity.BookReply;
 import com.trembear.bookinfoapi.dto.BookDto;
@@ -50,7 +56,28 @@ public class BookinfoApplicationTests {
 //        BeanUtils.copyProperties(bf,bt);
 //        System.out.println(bf.getContent());
 //        System.out.println(bt.getContent());
-        bcAdminMapper.selectByPrimaryKey(1);
+
+//        for(int i=3;i<20;i++){
+//            BCAdmin bcAdmin=new BCAdmin();
+//            bcAdmin.setAdminid(i);
+//            bcAdmin.setAdminname("admin"+i);
+//            bcAdmin.setCreateby(0);
+//            bcAdmin.setIsdelete("0");
+//            bcAdmin.setIsused("1");
+//            bcAdmin.setPassword("admin");
+//            bcAdmin.setSalt("salt");
+//            bcAdmin.setType("0");
+//            bcAdminMapper.insert(bcAdmin);
+//        }
+
+
+        PageHelper.startPage(1, 5);
+        List<BCAdmin> list= bcAdminMapper.selectByExample(new BCAdminExample());
+        PageInfo pageInfo = new PageInfo(list);
+        Page page = (Page) list;
+        System.out.println("PageInfo: " + JSON.toJSONString(pageInfo));
+        System.out.println("PageInfo: " + JSON.toJSONString(page));
+
     }
     //    @Test
     public void contextLoads() {
